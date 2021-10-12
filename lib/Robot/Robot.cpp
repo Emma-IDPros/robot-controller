b81ff8a27e5e91141d1e540f51af04ac29f4a5ad
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-12 22:16:38 +0100

    removed class defs

--- lib/Robot/Robot.cpp
+++ lib/Robot/Robot.cpp
@@ -3,20 +3,7 @@
 #include <SPI.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
-
-
-class Robot {
-public:
-	Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-	Adafruit_DCMotor* Motor1 = AFMS.getMotor(3);
-	Adafruit_DCMotor* Motor2 = AFMS.getMotor(4);
-
-	void move(uint8_t motor_number, uint8_t speed, uint8_t DIRECTION);
-	void move_all(uint8_t speed, uint8_t DIRECTION);
-	void release(uint8_t motor_number);
-	void release_all();
-	Adafruit_DCMotor* motor_from_motor_number(uint8_t motor_number);
-};
+#include <Robot.h>
 
 void Robot::move(uint8_t motor_number, uint8_t speed, uint8_t DIRECTION) {
 	Adafruit_DCMotor* motor = motor_from_motor_number(motor_number);
