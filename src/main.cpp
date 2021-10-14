592f2209c4b352b3c70cf1e6cd8efb1e300c3073
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-14 10:56:13 +0100

    WiFi debug selector

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 #include "RobotSensors.h"
 #include "configs.h"
 #include "IMU.h"
-// #include "WiFiComms.h"
+// #define WIFI_DEBUG
+
+#ifdef WIFI_DEBUG
+#include "WiFiComms.h"
+WiFiComms WiFiComm;
+#endif
 
 Robot Bot;
 RobotSensors Sensors;
 RobotIMU imu;
-// WiFiComms WiFiComm;
 
 void setup() {
 
@@ -17,26 +21,24 @@ void setup() {
   Bot.MotorShieldTest(); // Test to see if board can be detected
   Sensors.SetPins(US_pinTrig, US_pinEcho, IR_A21pin, IR_A02pin);
   imu.Begin();
-  // WiFiComm.Connect();
+
+#ifdef WIFI_DEBUG
+  WiFiComm.Connect();
+#endif
 
 }
 
 void loop() {
 
   float ultrasound_distance = Sensors.Ultrasound.GetDistance();
-  Serial.print("Ultrasound Distance: ");
-  Serial.println(String(ultrasound_distance));
-  if (ultrasound_distance < 20) {
+  Serial.println("Ultrasound Distance: " + String(ultrasound_distance));
 
+  if (ultrasound_distance < 20) {
     Bot.StopAll();
   }
   else {
-
     Bot.MoveAll(255, BACKWARD);
   }
-  delay(500);
-
-  char buffer[8];    // string buffer for use with dtostrf() function
 
   imu.ReadAcceleration();
   Serial.print("ax = ");  Serial.print(String(imu.ax));  Serial.print(" m/s^2, ");
@@ -44,6 +46,9 @@ void loop() {
   Serial.print("az = ");  Serial.print(String(imu.az));  Serial.print(" m/s^2, ");
   Serial.println(" ");
 
-  // WiFiComm.SendCoords(imu.ax, imu.ay);
+#ifdef WIFI_DEBUG
+  WiFiComm.SendCoords(imu.ax, imu.ay);
+#endif
 
+  delay(500);
 }
