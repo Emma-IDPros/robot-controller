721d47b9a1c7410a9c63c31dc0dc3d697dff28ed
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-15 16:55:28 +0100

    doc strings for methods

--- lib/IMU/IMU.cpp
+++ lib/IMU/IMU.cpp
@@ -1,6 +1,10 @@
 #include <IMU.h>
 #include <Arduino_LSM6DS3.h>
 
+/**
+ * @brief Reads the acceleration from the IMU and stores it in the attributes
+ * ax, ay, ax
+ */
 void RobotIMU::ReadAcceleration() {
 	if (IMU.accelerationAvailable()) {
 		IMU.readAcceleration(ax, ay, az);
@@ -10,13 +14,20 @@ void RobotIMU::ReadAcceleration() {
 	}
 }
 
+/**
+ * @brief Begins the connection to the IMU
+ */
 void RobotIMU::Begin() {
 	if (!IMU.begin()) {
 		Serial.println("Failed to initialize IMU!");
 		while (1);
 	}
 }
-
+/**
+ * @brief Attempt at performing numerical intergration of acceleration to give
+ * velocity and displacement. The velocity and displacement are stored in the
+ * attributes as vx, vy, vz and x, y, z
+ */
 void RobotIMU::Integrate() {
 	double delta_t = (millis() - prevMilliSeconds) / 1000;
 	vx += ax * delta_t; vy += ay * delta_t; vz += az * delta_t;
