9782698ea32a32e0fbe190620fa04eba900a71ac
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-15 16:57:11 +0100

    doc strings for headers

--- lib/IMU/IMU.h
+++ lib/IMU/IMU.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <Arduino_LSM6DS3.h>
 
+/**
+ * @brief Class for the Inertial Measurement Unit
+ */
 class RobotIMU {
 public:
+	// IMU acceleration
 	float ax, ay, az;
+	// velocity
 	float vx, vy, vz;
+	// positon
 	float x, y, z;
 	void ReadAcceleration();
 	void Begin();
