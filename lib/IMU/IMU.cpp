f9acbc48fe3f6c922f507a13b4da1190ae5871d9
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-15 21:06:14 +0100

    trapezium integration

--- lib/IMU/IMU.cpp
+++ lib/IMU/IMU.cpp
@@ -30,12 +30,12 @@ void RobotIMU::Begin() {
  */
 void RobotIMU::Integrate() {
 	double delta_t = (millis() - prevMilliSeconds) / 1000;
-	vx = TrapeziumArea(prev_ax, ax, delta_t);
-	vy = TrapeziumArea(prev_ay, ay, delta_t);
-	vz = TrapeziumArea(prev_az, az, delta_t);
-	x = TrapeziumArea(prev_vx, vx, delta_t);
-	y = TrapeziumArea(prev_vy, vy, delta_t);
-	z = TrapeziumArea(prev_vz, vz, delta_t);
+	vx += TrapeziumArea(prev_ax, ax, delta_t);
+	vy += TrapeziumArea(prev_ay, ay, delta_t);
+	vz += TrapeziumArea(prev_az, az, delta_t);
+	x += TrapeziumArea(prev_vx, vx, delta_t);
+	y += TrapeziumArea(prev_vy, vy, delta_t);
+	z += TrapeziumArea(prev_vz, vz, delta_t);
 
 	prev_ax = ax; prev_ay = ay; prev_az = az;
 	prev_vx = vx; prev_vy = vy; prev_vz = vz;
