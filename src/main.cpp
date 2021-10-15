b4500acfa9d6b74412b592c62667db8fe583d481
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-15 10:30:34 +0100

    Wifi Logging and renames

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,14 @@ WiFiComms WiFiComm;
 
 Robot Bot;
 RobotSensors Sensors;
-RobotIMU imu;
+RobotIMU BotIMU;
 
 void setup() {
 
   Serial.begin(9600);
-
   Bot.MotorShieldTest(); // Test to see if board can be detected
   Sensors.SetPins(US_pinTrig, US_pinEcho, IR_A21pin, IR_A02pin);
-  imu.Begin();
+  BotIMU.Begin();
 
 #ifdef WIFI_DEBUG
   WiFiComm.Connect();
@@ -31,9 +30,6 @@ void loop() {
 
   float ultrasound_distance = Sensors.Ultrasound.GetDistance();
   Serial.println("Ultrasound Distance: " + String(ultrasound_distance));
-#ifdef WIFI_DEBUG
-  WiFiComm.Message("Ultrasound Distance: " + String(ultrasound_distance));
-#endif
 
   if (ultrasound_distance < 20) {
     Bot.StopAll();
@@ -42,14 +38,13 @@ void loop() {
     Bot.MoveAll(255, BACKWARD);
   }
 
-  imu.ReadAcceleration();
-  Serial.print("ax = ");  Serial.print(String(imu.ax));  Serial.print(" m/s^2, ");
-  Serial.print("ay = ");  Serial.print(String(imu.ay));  Serial.print(" m/s^2, ");
-  Serial.print("az = ");  Serial.print(String(imu.az));  Serial.print(" m/s^2, ");
-  Serial.println(" ");
+  BotIMU.ReadAcceleration();
+  Serial.println("ax = " + String(BotIMU.ax) + " m/s^2, " + "ay = " + String(BotIMU.ay) + " m/s^2, " + "az = " + String(BotIMU.az) + " m/s^2, ");
 
 #ifdef WIFI_DEBUG
-  WiFiComm.SendCoords(imu.ax, imu.ay);
+  WiFiComm.Message("Ultrasound Distance: " + String(ultrasound_distance));
+  WiFiComm.Message("ax = " + String(BotIMU.ax) + " m/s^2, " + "ay = " + String(BotIMU.ay) + " m/s^2, " + "az = " + String(BotIMU.az) + " m/s^2, ");
+  WiFiComm.SendCoords(BotIMU.ax, BotIMU.ay);
 #endif
 
   delay(500);
