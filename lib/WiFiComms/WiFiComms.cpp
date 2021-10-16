d178963404df02fc604bbb1ac0b6d91b19e0d17e
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-16 12:17:41 +0100

    more doc strings

--- lib/WiFiComms/WiFiComms.cpp
+++ lib/WiFiComms/WiFiComms.cpp
@@ -4,6 +4,10 @@
 #include <Arduino.h>
 // #define WIFI_EVALUATE_STATUS_CODES
 
+/**
+ * @brief Connect to the WiFi hotspot on Lakee's computer
+ *
+ */
 void WiFiComms::Connect() {
 	int i = 0;
 	while (wl_status != WL_CONNECTED) {
@@ -32,13 +36,19 @@ void WiFiComms::Connect() {
 	}
 }
 
+/**
+ * @brief Makes a GET request to a specfic endpoint
+ * on the logging server
+ *
+ * @param endpoint the URL endpoint for the request
+ */
 void WiFiComms::Get(String endpoint) {
 	client.beginRequest();
 	String base_url = "http://" + String("192.168.137.1") + ":" + String(6969);
 	client.get(base_url + endpoint);
 	client.endRequest();
 
-#ifdef WIFI_EVALUATE_STATUS_CODES
+#ifdef WIFI_EVALUATE_STATUS_CODES // note: evaluating status codes introudes a 2s delay
 	int status_code = client.responseStatusCode();
 	if (status_code == 404) {
 		Serial.println("404 Error in making request to " + endpoint);
@@ -47,10 +57,22 @@ void WiFiComms::Get(String endpoint) {
 
 }
 
+/**
+ * @brief Sends coordinate data to the server
+ *
+ * @param x X coordinate
+ * @param y Y coordinate
+ */
 void WiFiComms::SendCoords(float x, float y) {
 	Get("/coord/" + String(x) + "/" + String(y));
 }
 
+/**
+ * @brief Sends a message to the message endpoint on the server
+ *
+ * @param message the message you want to send
+ * !!ensure that there are no slashes!!
+ */
 void WiFiComms::Message(String message) {
 	message.replace(" ", "%20");
 	Get("/message/" + message);
