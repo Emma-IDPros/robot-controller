86c1dacdf4322a769290181a7d020825dd008e66
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-19 10:01:00 +0100

    more attributes

--- lib/IMU/IMU.h
+++ lib/IMU/IMU.h
@@ -8,17 +8,21 @@ class RobotIMU {
 public:
 	// IMU acceleration
 	float ax, ay, az;
+	// gyroscope
+	float gx, gy, gz;
+	// angles
+	float pitch, yaw, roll;
 	// velocity
 	float vx, vy, vz;
 	// positon
 	float x, y, z;
-	void ReadAcceleration();
-	void Begin();
+	void ReadAcceleration();g
+		void Begin();
 	void Integrate();
 
 	void VerletInt();
 	float new_position, prev_position, position, velocity;
-	
+
 private:
 	float prevMilliSeconds = millis();
 	float prev_ax, prev_ay, prev_az;
