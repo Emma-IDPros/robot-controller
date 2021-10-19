3eab86df010f9ffc21f24a0d505d9d4ae1870fb7
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-19 16:56:33 +0100

    line sensor pins

--- include/configs.h
+++ include/configs.h
@@ -7,4 +7,6 @@ extern const byte MD_pin_pulse = A0;
 extern const byte MD_pin_cap = A1;
 extern const byte MD_pin_LED1 = 12;
 extern const byte MD_pin_LED2 = 11;
+extern const byte line_pin_sense = 0; // analogue
+extern const byte line_detect_pin = 7; // digital
 // #define WIFI_DEBUG
