5a328970fa9cf0c747a706deddc42bbc4ec7aff3
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-31 18:07:11 +0000

    moved to private

--- lib/Robot/Robot.h
+++ lib/Robot/Robot.h
@@ -17,10 +17,6 @@ struct MotorStatus
 // Robot Class
 class Robot {
 public:
-	// Adafruit Motor Shield Object
-	Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-	Adafruit_DCMotor* MotorRight = AFMS.getMotor(3);
-	Adafruit_DCMotor* MotorLeft = AFMS.getMotor(4);
 
 	void MotorShieldTest();
 	void Move(MOTOR motor, uint8_t speed, uint8_t DIRECTION);
@@ -28,11 +24,14 @@ public:
 	void Stop(MOTOR motor);
 	void StopAll();
 	// void Rotate(ROTATION rotation, RobotLineSensor LineSensor);
-	float rotation_time;
-	static bool rotate_execute;
 private:
+	Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+	Adafruit_DCMotor* MotorRight = AFMS.getMotor(3);
+	Adafruit_DCMotor* MotorLeft = AFMS.getMotor(4);
 	Adafruit_DCMotor* MotorFromMotorNumber(MOTOR motor);
 	MotorStatus motor_status_left, motor_status_right;
 	bool ValidateMotorStatus(MOTOR motor, uint8_t speed, uint8_t DIRECTION);
+	float rotation_time;
+	static bool rotate_execute;
 };
 
