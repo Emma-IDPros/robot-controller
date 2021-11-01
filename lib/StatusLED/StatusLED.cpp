f78e5304d6db7658a21248631b1552b7ba4cd161
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-11-01 10:34:05 +0000

    fixes

--- lib/StatusLED/StatusLED.cpp
+++ lib/StatusLED/StatusLED.cpp
@@ -6,9 +6,10 @@ void RobotStatusLED::SetPins(byte S_LED_pin_inp) {
 }
 
 void RobotStatusLED::Blink(int frequency, bool on) {
-	if (millis() - prevMillis > 1 / frequency * 1000 && on) {
+	if (millis() - prevMillis > 1 / (float)frequency * 1000 && on) {
 		S_LED_state = S_LED_state == LOW ? HIGH : LOW;
 		digitalWrite(S_LED_pin, S_LED_state);
+		prevMillis = millis();
 	}
 	else if (!on) {
 		digitalWrite(S_LED_pin, LOW);
