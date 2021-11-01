4202d1283b28cebade0b6d84382cedb2c7a152ed
Jiajiong Liu <jl2199@cam.ac.uk>
2021-11-01 10:59:25 +0000

    stuffd

--- lib/RobotDecisions/RobotDecisions.h
+++ lib/RobotDecisions/RobotDecisions.h
@@ -6,14 +6,16 @@
 #include <RobotSensors.h>
 #include <PickUp.h>
 #include <IMU.h>
+#include <MetalDetector.h>
 
 enum REFIND_LINE_STAGES { ROTATE_LEFT, LEFT_TO_MIDDLE, ROTATE_RIGHT, RIGHT_TO_MIDDLE };
 
 class RobotDecisions {
 public:
     void FollowLine(Robot Bot, RobotLineSensor LineSensor, bool use_front_line = false);
-    void FollowLineWithWiFi(Robot Bot, RobotLineSensor LineSensor, WiFiComms WiFiComm);
+    void FollowLineWithWiFi(Robot Bot, RobotLineSensor LineSensor, bool use_front_line, WiFiComms WiFiComm);
     void BlockCollect(Robot Bot, RobotSensors Sensors, RobotPickUp PickUp, RobotIMU BotIMU, RobotLineSensor LineSensor);
+    void Junction(Robot Bot, RobotSensors Sensors, RobotPickUp PickUp, RobotIMU BotIMU, RobotLineSensor LineSensor, RobotMetalDetector MetalDetector);
 private:
     float rot_speed;
     bool rotate_left = false;
