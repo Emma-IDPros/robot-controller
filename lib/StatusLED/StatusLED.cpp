1c4c50b6fc906ef17371f3425ead94c151bf64f6
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-11-03 10:14:46 +0000

    metal detector LED

--- lib/StatusLED/StatusLED.cpp
+++ lib/StatusLED/StatusLED.cpp
@@ -5,9 +5,11 @@
  *
  * @param S_LED_pin_inp Amber led pin
  */
-void RobotStatusLED::SetPins(byte S_LED_pin_inp) {
+void RobotStatusLED::SetPins(byte S_LED_pin_inp, byte MD_LED_pin_inp) {
 	pinMode(S_LED_pin_inp, OUTPUT);
+	pinMode(MD_LED_pin_inp, OUTPUT);
 	S_LED_pin = S_LED_pin_inp;
+	MD_LED_pin = MD_LED_pin_inp;
 }
 
 /**
@@ -26,4 +28,7 @@ void RobotStatusLED::Blink(int frequency, bool on) {
 		digitalWrite(S_LED_pin, LOW);
 		S_LED_state = LOW;
 	}
+}
+void RobotStatusLED::LightUpMetalDetectorLED(bool on) {
+	digitalWrite(MD_LED_pin, on ? HIGH : LOW);
 }
--- lib/StatusLED/StatusLED.h
+++ lib/StatusLED/StatusLED.h
@@ -3,10 +3,11 @@
 
 class RobotStatusLED {
 public:
-	void SetPins(byte S_LED_pin_inp);
+	void SetPins(byte S_LED_pin_inp, byte MD_LED_pin);
 	void Blink(int frequency, bool on);
+	void LightUpMetalDetectorLED(bool on);
 private:
-	byte S_LED_pin;
+	byte S_LED_pin, MD_LED_pin;
 	unsigned long prevMillis = millis();
 	PinStatus S_LED_state = LOW;
 };
