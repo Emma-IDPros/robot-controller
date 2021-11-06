0c9d27c32a49002d79fc33cdca872eeac13974d6
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-11-06 14:31:41 +0000

    pin configs

--- include/configs.h
+++ include/configs.h
@@ -8,20 +8,20 @@
  */
 
 #pragma once
-extern const byte IR_A02pin = 0;//must be analog
-extern const byte IR_A21pin = 3;//must be analog
-extern const byte US_pinTrig = 3;//must be digital
-extern const byte US_pinEcho = 2;//must be digital
-extern const byte MD_pin_pulse = 2;
-extern const byte MD_pin_cap = 1;
-extern const byte MD_pin_LED1 = 12;
-extern const byte MD_pin_LED2 = 11;
-extern const byte line_pin_sense = 2; // analogue
+extern const byte IR_A02pin = 0;// analog
+extern const byte IR_A21pin = 3;// analog
+extern const byte US_pinTrig = 3;// digital
+extern const byte US_pinEcho = 2;// digital
+extern const byte MD_pin_pulse = 2; // digital
+extern const byte MD_pin_cap = 1; // analog
+extern const byte MD_pin_LED1 = 12; // digital
+extern const byte MD_pin_LED2 = 11; // digutal
+extern const byte line_pin_sense = 2; // analog
 extern const byte line_detect_pin = 7; // digital
 extern const byte line_junc_detect_pin = 6; // digital
-extern const byte PU_servo_pin = 10;
-extern const byte amber_led_pin = 5;
-extern const byte T_detect_pin = 2;
-extern const byte T_led_pin = 13;
-extern const byte MD_led_pin_on = 9;
-extern const byte MD_led_pin_off = 8;
+extern const byte PU_servo_pin = 10; // digital
+extern const byte amber_led_pin = 5; // digital
+extern const byte T_detect_pin = 2; // digital
+extern const byte T_led_pin = 13; // digital
+extern const byte MD_led_pin_on = 9; // digital
+extern const byte MD_led_pin_off = 8; // digital
